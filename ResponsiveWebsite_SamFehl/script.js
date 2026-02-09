4c0ef5e17002b1273b60bb8d09638bbb36345166
SamFehl <sjfehl@pointpark.edu>
2026-02-08 22:44:56 -0500

    Fixing nested folder

---
+++ ResponsiveWebsite_SamFehl/script.js
@@ -0,0 +1,20 @@
+const shopButton = document.getElementById("shopBtn");
+const modal = document.getElementById("modal");
+const closeBtn = document.querySelector(".close-btn");
+
+// Open modal
+shopButton.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+// Close modal (X button)
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+// Close modal when clicking outside
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
